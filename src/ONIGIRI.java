3c081de05985cbd347b44733f730118b2e08900d
0Alina <alina.glodea@gmail.com>
2023-10-30 20:10:18 +0200

    crearea claselor SAKURA/ONIGIRI

--- src/ONIGIRI.java
+++ src/ONIGIRI.java
@@ -1,2 +1,16 @@
 public class ONIGIRI {
+    private String umplutura;
+    ONIGIRI (String umplutura) {
+        this.umplutura = umplutura;
+    }
+    public String getUmplutura() {
+        return umplutura;
+    }
+    public void setUmplutura(String umplutura) {
+        this.umplutura = umplutura;
+    }
+    @Override
+    public String toString() {
+        return "ONIGIRI{umplutura='"+umplutura+"'}";
+    }
 }
--- src/SAKURA.java
+++ src/SAKURA.java
@@ -1,2 +1,16 @@
 public class SAKURA {
+    private String specie;
+    SAKURA (String specie) {
+        this.specie = specie;
+    }
+    public String getSpecie() {
+        return specie;
+    }
+    public void setSpecie() {
+        this.specie = specie;
+    }
+    @Override
+    public String toString() {
+        return "SAKURA{specie='"+specie+"'}";
+    }
 }
